9872160d6d2cdddf1ad500c490fd97f7e22be935
MinyazevR <89993880+MinyazevR@users.noreply.github.com>
2025-11-15 16:11:39 +0300

    Add generated files for Qt5.10.1 for old TRIK firmware (#6)
    Add generated files for Qt5.10.1 for old TRIK firmware

---
+++ generated_cpp_5.10.1/com_trolltech_qt_core/com_trolltech_qt_core4.cpp
@@ -0,0 +1,8 @@
+#include "com_trolltech_qt_core4.h"
+#include <PythonQtConversion.h>
+#include <PythonQtMethodInfo.h>
+#include <PythonQtSignalReceiver.h>
+#include <QVariant>
+#include <qlogging.h>
+
+
